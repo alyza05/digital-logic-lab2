a65e9cb29ab7ab97c8e00cbbdcf17828387528a1
alyza05 <alyzaalfonso@gmail.com>
2025-02-26 19:11:02 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -8,7 +8,7 @@ module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
-    $dumpfile("digital_logic_lab2.vcd");
+     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
